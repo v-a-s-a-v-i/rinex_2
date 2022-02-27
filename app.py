54a48bfaef8f5b2d4aeb3a049c3e081d10b72cfb
v-a-s-a-v-i <100505394+v-a-s-a-v-i@users.noreply.github.com>
2022-02-27 12:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ ip = st.text_input('Enter your message')
 op = model.predict([ip])
 if st.button('Predict'):
   st.title(po[0])
+                                       
                                                     
                                            
                                                                                               
